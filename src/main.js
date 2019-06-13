dff020b747196b1b8b17a689916d9817ee4a2b92
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2019-06-13 05:29:46 +0200

    Mitele funcionando, no todos los videos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 function ajax_request() {
     $.ajax({
-        url: "https://cors-anywhere.herokuapp.com/https://www.mitele.es/series-online/brigada-costa-del-sol/5cfba6688bd4e51f0d8b46a3/player/",
+        url: "https://cors-anywhere.herokuapp.com/" + $("input#url").val() ,
         type: 'GET',
         dataType: 'html',
         success: function (data) {
@@ -8,6 +8,8 @@ function ajax_request() {
             var ting = headline.split("_");
             document.getElementById("content").innerHTML = "ID catálogo: " + ting[1];
             document.getElementById("content2").innerHTML = "ID video: " + ting[2];
+            /*$("source#videosrc").attr("src", "https://miteleooyala-vh.akamaihd.net/i/" + ting[2] + "/,DOcJ-FxaFrRg4gtDEwOjFsaTowODE7WX,DOcJ-FxaFrRg4gtDEwOm84OjBrOz0bxa,DOcJ-FxaFrRg4gtDEwOjEwYjowODE7je,DOcJ-FxaFrRg4gtDEwOjJjcTowODE7R4,.csmil/index_2_av.m3u8?null=0&hdntl=exp=1560305295~acl=%2fi%2f9yOXl4aDE6bYacKb5FtcCbYZUmNI96SY%2f,DOcJ-FxaFrRg4gtDEwOjFsaTowODE7WX,DOcJ-FxaFrRg4gtDEwOm84OjBrOz0bxa,DOcJ-FxaFrRg4gtDEwOjEwYjowODE7je,DOcJ-FxaFrRg4gtDEwOjJjcTowODE7R4,.csmil%2f*~data=hdntl~hmac=0c5d6b0d7ef48605be0df09516f7dae3c15d3a12409b36de92f5cc0de5e23bb6");*/
+            player.src("https://miteleooyala-vh.akamaihd.net/i/" + ting[2] + "/,DOcJ-FxaFrRg4gtDEwOjFsaTowODE7WX,DOcJ-FxaFrRg4gtDEwOm84OjBrOz0bxa,DOcJ-FxaFrRg4gtDEwOjEwYjowODE7je,DOcJ-FxaFrRg4gtDEwOjJjcTowODE7R4,.csmil/index_2_av.m3u8?null=0&hdntl=exp=1560305295~acl=%2fi%2f9yOXl4aDE6bYacKb5FtcCbYZUmNI96SY%2f,DOcJ-FxaFrRg4gtDEwOjFsaTowODE7WX,DOcJ-FxaFrRg4gtDEwOm84OjBrOz0bxa,DOcJ-FxaFrRg4gtDEwOjEwYjowODE7je,DOcJ-FxaFrRg4gtDEwOjJjcTowODE7R4,.csmil%2f*~data=hdntl~hmac=0c5d6b0d7ef48605be0df09516f7dae3c15d3a12409b36de92f5cc0de5e23bb6")
             
         }
     });
