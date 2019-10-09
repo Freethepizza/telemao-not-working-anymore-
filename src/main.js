ce417d2616f79847507b1f9377e2fd2b558949af
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2019-10-09 08:20:43 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ function ajax_request() {
     if (hostname=="www.atresplayer.com" ){
         var id = url.substr(url.length - 25);
       $.ajax({
-        url: "https://api.atresplayer.com/client/v1/player/video/" + id,
+        url: "https://jsonp.afeld.me/?url=https://api.atresplayer.com/client/v1/player/video/" + id,
         type: 'GET',
         dataType: 'json',
         success: function (data) {
